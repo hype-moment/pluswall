a33418b632611f4587a42d57703e2ecb56d14bbd
Diego Cesare <cesarediego18@gmail.com>
2020-10-17 15:49:30 -0300

    Corrigido problema de compatibilidade em multiplas WM's

--- PlusWall/Arch/plus.sh
+++ PlusWall/Arch/plus.sh
@@ -5,8 +5,8 @@ GetWmName(){
 num=$(wmctrl -m | grep Name | sed 's/.*: //g')
 
 #### Get workspace herbstluftwm
-if [[ $num == herbstluftwm ]]; then
-herbstclient list_monitors | awk '{print $5}' | sed 's/"//g'
+if [[ "$num" == herbstluftwm ]]; then
+wmctrl -d | grep '*' | awk '{print $1 $2}' | sed 's/*/-/'
 ### Get workspace i3
 elif [[ $num == i3 ]]; then
 i3-msg -t get_workspaces | jq '.[] | select(.focused==true).name'| cut -d"\"" -f2
@@ -15,19 +15,19 @@ elif [[ $num == bspwm ]]; then
 bspc query -D -d focused --names
 ### Get workspace qtile
 elif [[ $num == LG3D ]]; then
-wmctrl -d | grep '*' | awk '{print $1 $2}' | sed 's/*/0/'
+wmctrl -d | grep '*' | awk '{print $1 $2}' | sed 's/*/-/'
 ### Get workspace openbox
 elif [[ $num == Openbox ]]; then
-wmctrl -d | grep '*' | awk '{print $1 $2}' | sed 's/*/0/'
+wmctrl -d | grep '*' | awk '{print $1 $2}' | sed 's/*/-/'
 ### Get workspace fluxbox
 elif [[ $num == Fluxbox ]]; then
-wmctrl -d | grep '*' | awk '{print $1 $2}' | sed 's/*/0/'
+wmctrl -d | grep '*' | awk '{print $1 $2}' | sed 's/*/-/'
 ### Get workspace jwm
 elif [[ $num == JWM ]]; then
-wmctrl -d | grep '*' | awk '{print $1 $2}' | sed 's/*/0/'
+wmctrl -d | grep '*' | awk '{print $1 $2}' | sed 's/*/-/'
 ### Get workspace pekwm
 elif [[ $num == pekwm ]]; then
-wmctrl -d | grep '*' | awk '{print $1 $2}' | sed 's/*/0/'
+wmctrl -d | grep '*' | awk '{print $1 $2}' | sed 's/*/-/'
 fi
 }
 
@@ -39,16 +39,16 @@ GetWmName
 
 case $(GetWmName) in
 
-00|I|1) feh --bg-fill ~/PlusWall/Arch/0.png 	;;
-10|II|2) feh --bg-fill ~/PlusWall/Arch/1.png 	;;
-20|III|3) feh --bg-fill ~/PlusWall/Arch/2.png 	;;
-30|IV|4) feh --bg-fill ~/PlusWall/Arch/3.png 	;;
-40|V|5) feh --bg-fill ~/PlusWall/Arch/4.png	;;
-50|VI|6) feh --bg-fill ~/PlusWall/Arch/5.png	;;
-60|VII|7) feh --bg-fill ~/PlusWall/Arch/6.png	;;
-70|VIII|8) feh --bg-fill ~/PlusWall/Arch/7.png	;;
-80|IX|9) feh --bg-fill ~/PlusWall/Arch/8.png	;;
-90|X|10) feh --bg-fill ~/PlusWall/Arch/9.png 	;;
+0-|I|1) feh --bg-fill ~/PlusWall/Arch/0.png 	;;
+1-|II|2) feh --bg-fill ~/PlusWall/Arch/1.png 	;;
+2-|III|3) feh --bg-fill ~/PlusWall/Arch/2.png 	;;
+3-|IV|4) feh --bg-fill ~/PlusWall/Arch/3.png 	;;
+4-|V|5) feh --bg-fill ~/PlusWall/Arch/4.png	;;
+5-|VI|6) feh --bg-fill ~/PlusWall/Arch/5.png	;;
+6-|VII|7) feh --bg-fill ~/PlusWall/Arch/6.png	;;
+7-|VIII|8) feh --bg-fill ~/PlusWall/Arch/7.png	;;
+8-|IX|9) feh --bg-fill ~/PlusWall/Arch/8.png	;;
+9-|X|10) feh --bg-fill ~/PlusWall/Arch/9.png 	;;
 
 esac
 
--- PlusWall/plus-wall.sh
+++ PlusWall/plus-wall.sh
@@ -10,10 +10,11 @@ GetVersion(){
 
 wget -O status https://github.com/hype-moment/PlusWall/blob/main/PlusWall/plus-wall.sh 2> /dev/null
 
+#Version=$(cat status | grep Version | sed 's/[-=#" a-z A-Z <>/;"&"(){}\$[]//g;s/.*\.//g' | sed -n 1p)
 Version=$(cat status | grep "Version = " | sed 's/.* = //g;s/<\/.*//g' | sed -n 1p)
-
 if [[ $Version > 1.0 ]]; then
-	echo -e "\e[1;32mNew version available\e[m\n\e[1;31mCheck the news at\e[m\nhttps://github.com/hype-moment/PlusWall"
+	echo -e "\e[1;32mNew version available \e[1;36m$Version\e[m\e[m\n\e[1;31mCheck the news at\e[m\nhttps://github.com/hype-moment/PlusWall"
+	echo -e "New category \e[1;4;31mHero\e[m"
 fi
 
 rm status 2> /dev/null
@@ -32,8 +33,8 @@ echo -e "━━━━━━━━━━━━━━━━━━━━━━━�
 
 echo -e "1) Animes   6) Gentoo    11) LOL      16) FakeBlur-MX"
 echo -e "2) Aliens   7) Linux     12) Nature   17) FakeBlur-Jinx"
-echo -e "3) Cars     8) Panorama  13) Soon     18) FakeBlur-No Man's Sky"
-echo -e "4) Arch     9) Surreal   14) Soon     19) Soon"
+echo -e "3) Cars     8) Panorama  13) Girls    18) FakeBlur-No Man's Sky"
+echo -e "4) Arch     9) Surreal   14) Hero     19) Soon"
 echo -e "5) Debian  10) Simple    15) Soon     20) Soon"
 echo -e "▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂"
 echo -e "Restart the session for the effects to be applied."
@@ -54,6 +55,8 @@ case $opc in
    10) sed -i 's/l\/.*/l\/Simple\/plus.sh \&/' ~/.profile 	;;
    11) sed -i 's/l\/.*/l\/LOL\/plus.sh \&/' ~/.profile 		;;
    12) sed -i 's/l\/.*/l\/Nature\/plus.sh \&/' ~/.profile ;;
+   13) sed -i 's/l\/.*/l\/Girls\/plus.sh \&/' ~/.profile ;;
+   14) sed -i 's/l\/.*/l\/Hero\/plus.sh \&/' ~/.profile ;;
    16) sed -i 's/l\/.*/l\/FakeBlur\/fakeblur.sh \&/' ~/.profile 	;;
    17) sed -i 's/l\/.*/l\/FakeBlur\/fakeblurjinx.sh \&/' ~/.profile	;;
    18) sed -i 's/l\/.*/l\/FakeBlur\/fakeblursky.sh \&/' ~/.profile	;;
