065fe92b9d2be1535c87b93052c89e779ddc1fbf
Diego Cesare <cesarediego18@gmail.com>
2020-10-20 19:53:17 -0300

    Nova categoria 'TimeSmile'

--- PlusWall/TimeSmile/Smile.sh
+++ PlusWall/TimeSmile/Smile.sh
@@ -5,128 +5,128 @@ while true; do
 time=$(date '+%S')
 
 	if [[ $time = 00 ]]; then
-		feh --bg-scale ~/Projeto-Fazes_do_dia/0.png
+		feh --bg-scale ~/PlusWall/TimeSmile/0.png
 	elif [[ $time = 01 ]]; then
-    	feh --bg-scale ~/Projeto-Fazes_do_dia/1.png
+    	feh --bg-scale ~/PlusWall/TimeSmile/1.png
     elif [[ $time = 02 ]]; then
-    	feh --bg-scale ~/Projeto-Fazes_do_dia/2.png
+    	feh --bg-scale ~/PlusWall/TimeSmile/2.png
     elif [[ $time = 03 ]]; then
-    	feh --bg-scale ~/Projeto-Fazes_do_dia/3.png
+    	feh --bg-scale ~/PlusWall/TimeSmile/3.png
     elif [[ $time = 04 ]]; then
-    	feh --bg-scale ~/Projeto-Fazes_do_dia/4.png
+    	feh --bg-scale ~/PlusWall/TimeSmile/4.png
     elif [[ $time = 05 ]]; then
-    	feh --bg-scale ~/Projeto-Fazes_do_dia/5.png
+    	feh --bg-scale ~/PlusWall/TimeSmile/5.png
     elif [[ $time = 06 ]]; then
-    	feh --bg-scale ~/Projeto-Fazes_do_dia/6.png
+    	feh --bg-scale ~/PlusWall/TimeSmile/6.png
     elif [[ $time = 07 ]]; then
-    	feh --bg-scale ~/Projeto-Fazes_do_dia/7.png
+    	feh --bg-scale ~/PlusWall/TimeSmile/7.png
     elif [[ $time = 08 ]]; then
-    	feh --bg-scale ~/Projeto-Fazes_do_dia/8.png
+    	feh --bg-scale ~/PlusWall/TimeSmile/8.png
     elif [[ $time = 09 ]]; then
-    	feh --bg-scale ~/Projeto-Fazes_do_dia/9.png
+    	feh --bg-scale ~/PlusWall/TimeSmile/9.png
     elif [[ $time = 10 ]]; then
-    	feh --bg-scale ~/Projeto-Fazes_do_dia/10.png
+    	feh --bg-scale ~/PlusWall/TimeSmile/10.png
     elif [[ $time = 11 ]]; then
-    	feh --bg-scale ~/Projeto-Fazes_do_dia/11.png
+    	feh --bg-scale ~/PlusWall/TimeSmile/11.png
     elif [[ $time = 12 ]]; then
-    	feh --bg-scale ~/Projeto-Fazes_do_dia/12.png
+    	feh --bg-scale ~/PlusWall/TimeSmile/12.png
     elif [[ $time = 13 ]]; then
-    	feh --bg-scale ~/Projeto-Fazes_do_dia/13.png
+    	feh --bg-scale ~/PlusWall/TimeSmile/13.png
     elif [[ $time = 14 ]]; then
-    	feh --bg-scale ~/Projeto-Fazes_do_dia/14.png
+    	feh --bg-scale ~/PlusWall/TimeSmile/14.png
     elif [[ $time = 15 ]]; then
-    	feh --bg-scale ~/Projeto-Fazes_do_dia/15.png
+    	feh --bg-scale ~/PlusWall/TimeSmile/15.png
     elif [[ $time = 16 ]]; then
-    	feh --bg-scale ~/Projeto-Fazes_do_dia/16.png
+    	feh --bg-scale ~/PlusWall/TimeSmile/16.png
     elif [[ $time = 17 ]]; then
-    	feh --bg-scale ~/Projeto-Fazes_do_dia/17.png
+    	feh --bg-scale ~/PlusWall/TimeSmile/17.png
     elif [[ $time = 18 ]]; then
-    	feh --bg-scale ~/Projeto-Fazes_do_dia/18.png
+    	feh --bg-scale ~/PlusWall/TimeSmile/18.png
     elif [[ $time = 19 ]]; then
-    	feh --bg-scale ~/Projeto-Fazes_do_dia/19.png
+    	feh --bg-scale ~/PlusWall/TimeSmile/19.png
     elif [[ $time = 20 ]]; then
-    	feh --bg-scale ~/Projeto-Fazes_do_dia/20.png
+    	feh --bg-scale ~/PlusWall/TimeSmile/20.png
     elif [[ $time = 21 ]]; then
-    	feh --bg-scale ~/Projeto-Fazes_do_dia/21.png
+    	feh --bg-scale ~/PlusWall/TimeSmile/21.png
     elif [[ $time = 22 ]]; then
-    	feh --bg-scale ~/Projeto-Fazes_do_dia/22.png
+    	feh --bg-scale ~/PlusWall/TimeSmile/22.png
     elif [[ $time = 23 ]]; then
-    	feh --bg-scale ~/Projeto-Fazes_do_dia/23.png
+    	feh --bg-scale ~/PlusWall/TimeSmile/23.png
     elif [[ $time = 24 ]]; then
-        feh --bg-scale ~/Projeto-Fazes_do_dia/24.png
+        feh --bg-scale ~/PlusWall/TimeSmile/24.png
     elif [[ $time = 25 ]]; then
-        feh --bg-scale ~/Projeto-Fazes_do_dia/25.png
+        feh --bg-scale ~/PlusWall/TimeSmile/25.png
     elif [[ $time = 26 ]]; then
-        feh --bg-scale ~/Projeto-Fazes_do_dia/26.png
+        feh --bg-scale ~/PlusWall/TimeSmile/26.png
     elif [[ $time = 27 ]]; then
-        feh --bg-scale ~/Projeto-Fazes_do_dia/27.png
+        feh --bg-scale ~/PlusWall/TimeSmile/27.png
     elif [[ $time = 28 ]]; then
-        feh --bg-scale ~/Projeto-Fazes_do_dia/28.png
+        feh --bg-scale ~/PlusWall/TimeSmile/28.png
     elif [[ $time = 29 ]]; then
-        feh --bg-scale ~/Projeto-Fazes_do_dia/29.png
+        feh --bg-scale ~/PlusWall/TimeSmile/29.png
     elif [[ $time = 30 ]]; then
-        feh --bg-scale ~/Projeto-Fazes_do_dia/30.png
+        feh --bg-scale ~/PlusWall/TimeSmile/30.png
 
     elif [[ $time = 31 ]]; then
-        feh --bg-scale ~/Projeto-Fazes_do_dia/29.png
+        feh --bg-scale ~/PlusWall/TimeSmile/29.png
     elif [[ $time = 32 ]]; then
-        feh --bg-scale ~/Projeto-Fazes_do_dia/28.png
+        feh --bg-scale ~/PlusWall/TimeSmile/28.png
     elif [[ $time = 33 ]]; then
-        feh --bg-scale ~/Projeto-Fazes_do_dia/27.png
+        feh --bg-scale ~/PlusWall/TimeSmile/27.png
     elif [[ $time = 34 ]]; then
-        feh --bg-scale ~/Projeto-Fazes_do_dia/26.png
+        feh --bg-scale ~/PlusWall/TimeSmile/26.png
     elif [[ $time = 35 ]]; then
-        feh --bg-scale ~/Projeto-Fazes_do_dia/25.png
+        feh --bg-scale ~/PlusWall/TimeSmile/25.png
     elif [[ $time = 36 ]]; then
-        feh --bg-scale ~/Projeto-Fazes_do_dia/24.png
+        feh --bg-scale ~/PlusWall/TimeSmile/24.png
     elif [[ $time = 37 ]]; then
-        feh --bg-scale ~/Projeto-Fazes_do_dia/23.png
+        feh --bg-scale ~/PlusWall/TimeSmile/23.png
     elif [[ $time = 38 ]]; then
-        feh --bg-scale ~/Projeto-Fazes_do_dia/22.png
+        feh --bg-scale ~/PlusWall/TimeSmile/22.png
     elif [[ $time = 39 ]]; then
-        feh --bg-scale ~/Projeto-Fazes_do_dia/21.png
+        feh --bg-scale ~/PlusWall/TimeSmile/21.png
     elif [[ $time = 40 ]]; then
-        feh --bg-scale ~/Projeto-Fazes_do_dia/20.png
+        feh --bg-scale ~/PlusWall/TimeSmile/20.png
     elif [[ $time = 41 ]]; then
-        feh --bg-scale ~/Projeto-Fazes_do_dia/19.png
+        feh --bg-scale ~/PlusWall/TimeSmile/19.png
     elif [[ $time = 42 ]]; then
-        feh --bg-scale ~/Projeto-Fazes_do_dia/18.png
+        feh --bg-scale ~/PlusWall/TimeSmile/18.png
     elif [[ $time = 43 ]]; then
-        feh --bg-scale ~/Projeto-Fazes_do_dia/17.png
+        feh --bg-scale ~/PlusWall/TimeSmile/17.png
     elif [[ $time = 44 ]]; then
-        feh --bg-scale ~/Projeto-Fazes_do_dia/16.png
+        feh --bg-scale ~/PlusWall/TimeSmile/16.png
     elif [[ $time = 45 ]]; then
-        feh --bg-scale ~/Projeto-Fazes_do_dia/15.png
+        feh --bg-scale ~/PlusWall/TimeSmile/15.png
     elif [[ $time = 46 ]]; then
-        feh --bg-scale ~/Projeto-Fazes_do_dia/14.png
+        feh --bg-scale ~/PlusWall/TimeSmile/14.png
     elif [[ $time = 47 ]]; then
-        feh --bg-scale ~/Projeto-Fazes_do_dia/13.png
+        feh --bg-scale ~/PlusWall/TimeSmile/13.png
     elif [[ $time = 48 ]]; then
-        feh --bg-scale ~/Projeto-Fazes_do_dia/12.png
+        feh --bg-scale ~/PlusWall/TimeSmile/12.png
     elif [[ $time = 49 ]]; then
-        feh --bg-scale ~/Projeto-Fazes_do_dia/11.png
+        feh --bg-scale ~/PlusWall/TimeSmile/11.png
     elif [[ $time = 50 ]]; then
-        feh --bg-scale ~/Projeto-Fazes_do_dia/10.png
+        feh --bg-scale ~/PlusWall/TimeSmile/10.png
     elif [[ $time = 51 ]]; then
-        feh --bg-scale ~/Projeto-Fazes_do_dia/9.png
+        feh --bg-scale ~/PlusWall/TimeSmile/9.png
     elif [[ $time = 52 ]]; then
-        feh --bg-scale ~/Projeto-Fazes_do_dia/8.png
+        feh --bg-scale ~/PlusWall/TimeSmile/8.png
     elif [[ $time = 53 ]]; then
-        feh --bg-scale ~/Projeto-Fazes_do_dia/7.png
+        feh --bg-scale ~/PlusWall/TimeSmile/7.png
     elif [[ $time = 54 ]]; then
-        feh --bg-scale ~/Projeto-Fazes_do_dia/6.png
+        feh --bg-scale ~/PlusWall/TimeSmile/6.png
     elif [[ $time = 55 ]]; then
-        feh --bg-scale ~/Projeto-Fazes_do_dia/5.png
+        feh --bg-scale ~/PlusWall/TimeSmile/5.png
     elif [[ $time = 56 ]]; then
-        feh --bg-scale ~/Projeto-Fazes_do_dia/4.png
+        feh --bg-scale ~/PlusWall/TimeSmile/4.png
     elif [[ $time = 57 ]]; then
-        feh --bg-scale ~/Projeto-Fazes_do_dia/3.png
+        feh --bg-scale ~/PlusWall/TimeSmile/3.png
     elif [[ $time = 58 ]]; then
-        feh --bg-scale ~/Projeto-Fazes_do_dia/3.png
+        feh --bg-scale ~/PlusWall/TimeSmile/3.png
     elif [[ $time = 59 ]]; then
-        feh --bg-scale ~/Projeto-Fazes_do_dia/1.png
+        feh --bg-scale ~/PlusWall/TimeSmile/1.png
     elif [[ $time = 60 ]]; then
-        feh --bg-scale ~/Projeto-Fazes_do_dia/0.png
+        feh --bg-scale ~/PlusWall/TimeSmile/0.png
     fi
 done
 
