6d3f01db413cc1e81da102a2a5664841826d37fc
Diego Cesare <cesarediego18@gmail.com>
2020-10-20 18:53:20 -0300

    Nova categorai 'TimeSmile'

--- PlusWall/plus-wall.sh
+++ PlusWall/plus-wall.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 #======================================================
-# Version = 1.0
+# Version = 1.1
 # Creator = Diego Cesare <diegocesare300491@gmail.com>
 # License = GPL v3
 #======================================================
@@ -14,7 +14,7 @@ wget -O status https://github.com/hype-moment/PlusWall/blob/main/PlusWall/plus-w
 Version=$(cat status | grep "Version = " | sed 's/.* = //g;s/<\/.*//g' | sed -n 1p)
 if [[ $Version > 1.0 ]]; then
 	echo -e "\e[1;32mNew version available \e[1;36m$Version\e[m\e[m\n\e[1;31mCheck the news at\e[m\nhttps://github.com/hype-moment/PlusWall"
-	echo -e "New category \e[1;4;31mHero\e[m"
+	echo -e "New category \e[1;4;31mTimeSmile\e[m"
 fi
 
 rm status 2> /dev/null
@@ -34,7 +34,7 @@ echo -e "━━━━━━━━━━━━━━━━━━━━━━━�
 echo -e "1) Animes   6) Gentoo    11) LOL      16) FakeBlur-MX"
 echo -e "2) Aliens   7) Linux     12) Nature   17) FakeBlur-Jinx"
 echo -e "3) Cars     8) Panorama  13) Girls    18) FakeBlur-No Man's Sky"
-echo -e "4) Arch     9) Surreal   14) Hero     19) Soon"
+echo -e "4) Arch     9) Surreal   14) Hero     19) Time-Smile"
 echo -e "5) Debian  10) Simple    15) Soon     20) Soon"
 echo -e "▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂"
 echo -e "Restart the session for the effects to be applied."
@@ -60,6 +60,7 @@ case $opc in
    16) sed -i 's/l\/.*/l\/FakeBlur\/fakeblur.sh \&/' ~/.profile 	;;
    17) sed -i 's/l\/.*/l\/FakeBlur\/fakeblurjinx.sh \&/' ~/.profile	;;
    18) sed -i 's/l\/.*/l\/FakeBlur\/fakeblursky.sh \&/' ~/.profile	;;
+   19) sed -i 's/l\/.*/l\/TimeSmile\/Smile.sh \&/' ~/.profile	;;
 esac	
 }
 
